1ca9185a5d3ce38a08516f148abe3115f0e548c7
mstpn <57071649+mstpn@users.noreply.github.com>
2023-03-16 21:25:25 -0600

    did the thing

--- prolog/scheduler.pl
+++ prolog/scheduler.pl
@@ -40,17 +40,65 @@ schedule_components(Schedule) :-
 schedule_components([Key|Keys], Acc, Schedule) :-
     get_components(Key, Components),
     % write(Components), nl,
-    append(Acc, [Components], Acc1),
+    add_course(Components, Acc, Acc1),
     schedule_components(Keys, Acc1, Schedule).
 schedule_components([], Schedule, Schedule).
 
 % pretty print out the schedule
 print_schedule([]).
 print_schedule([Course|Courses]) :-
-    write(Course.name), write(" "), write(Course.start), write(" "), write(Course.duration), nl,
+    % write(Course.name), write(" "), write(Course.start), write(" "), write(Course.duration), nl,
     print_schedule(Courses).
 
+% check if there are conflicts between a given course and the schedule
+no_conflict(_, []).
+no_conflict(Course, Schedule) :-
+    write("BEFORE no conflict/3"),nl,
+    % write("Course: "), write(Course), nl,
+    Start = Course.start,
+    Duration = Course.duration,
+    write("calling no_conflict/3"),nl,
+    no_conflict(Schedule, Start, Duration).
 
+no_conflict([], _, _).
+no_conflict([Course|Courses], Start, Duration) :-
+    % write("no conflict/3"),nl,
+    % write("Course: "), write(Course), nl,
+    % write("Start: "), write(Start), nl,
+    % write("Duration: "), write(Duration), nl,
+    % write("Course.start: "), write(Course.start), nl,
+    write("Course.duration: "), write(Course.duration), nl,
+    (
+        Start >= Course.start,
+        Start < Course.start + Course.duration
+    ;
+        Start + Duration > Course.start,
+        Start + Duration =< Course.start + Course.duration
+    ;
+        Start =< Course.start,
+        Start + Duration >= Course.start + Course.duration
+    ),
+    no_conflict(Courses, Start, Duration).
+
+% add a course to the schedule if there are no conflicts
+add_course(CourseComponents, Schedule, NewSchedule) :-
+    (no_conflict(CourseComponents, Schedule) 
+    -> 
+        write("Schedule: "), write(Schedule), nl,
+        write("CourseComponents: "), write(CourseComponents), nl,
+        write("NewSchedule: "), write(NewSchedule), nl,
+        append(Schedule, [CourseComponents], NewSchedule),
+        write("NewSchedule: "), write(NewSchedule), nl,
+        write("NO CONFLICOCJSO"),nl
+    ;
+        write("CONFLICT"),nl,
+        NewSchedule = Schedule
+    ).
+
+% (atom(Str)
+% -> Key = Str
+% ; 
+% atom_string(Key, Str)).
 
 
 
