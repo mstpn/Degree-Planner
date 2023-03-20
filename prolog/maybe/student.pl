a222721409375e08b10b239c4520d2189575b165
SorenSawback <97757105+SorenSawback@users.noreply.github.com>
2023-03-20 15:44:30 -0600

    Yup

---
+++ prolog/maybe/student.pl
@@ -0,0 +1,22 @@
+student(
+    name(soren),
+    sem(fall),
+    courses_taken(
+            [
+                "COMP1631","COMP1633"
+            ]
+            ),
+    chosen_jun_options(
+            [
+                
+            ]
+            ),
+    chosen_sen_options(            
+            [
+                               
+            ]
+        ),
+    cog(geog),
+    years_to_grad(8),
+    max_course(8)
+    ).
