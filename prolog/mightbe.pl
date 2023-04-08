62dfba5cd3a50cc7a826d25c0bca45d439e83243
SorenSawback <97757105+SorenSawback@users.noreply.github.com>
2023-04-07 18:44:05 -0600

    Removed the pick predicate

--- prolog/mightbe.pl
+++ prolog/mightbe.pl
@@ -9,8 +9,7 @@ offering(comp2655, fall, d100, mwf(14), mru).
 offering(comp2631, fall, d100, tr2(10), mru).
 offering(comp2613, fall, d100, tr2(14), mru).
 offering(comp3659, fall, d100, mwf(8), mru).
-offering(comp3659, winter, d100, mwf(80), mru).
-offering(comp3659, fall, d100, mwf(80), mru).
+
 offering(comp2659, winter, d100, mwf(15), mru).
 offering(comp2633, winter, d100, tr2(8), mru).
 offering(math2234, winter, d100, mwf(10), mru).
@@ -33,55 +32,20 @@ offering(comp3649, winter, d100, tr2(10), mru).
 offering(comp3614, winter, d100, t2r(14), mru).
 
 % OPTIONS
-offering(comp2521, winter, d100, tr2(110), mru).
-offering(comp2521, winter, d100, t2r(214), mru).
-offering(comp2521, fall, d100, tr2(315), mru).
-offering(comp3533, winter, d100, mwf(410), mru).
-offering(comp3533, fall, d100, mwf(510), mru).
-offering(comp3625, fall, d100, t2r(620), mru).
-offering(comp3505, winter, d100, mwf(713), mru).
-offering(comp4555, fall, d100, mwf(810), mru).
-offering(comp4513, winter, d100, mwf(915), mru).
-offering(comp4522, winter, d100, mwf(1110), mru).
-offering(comp4630, winter, d100, mwf(2011), mru).
+offering(comp2521, winter, d100, tr2(10), mru).
+offering(comp2521, winter, d100, t2r(14), mru).
+offering(comp2521, fall, d100, tr2(15), mru).
+offering(comp3533, winter, d100, mwf(10), mru).
+offering(comp3533, fall, d100, mwf(10), mru).
+offering(comp3625, fall, d100, t2r(20), mru).
+offering(comp3505, winter, d100, mwf(13), mru).
+offering(comp4555, fall, d100, mwf(10), mru).
+offering(comp4513, winter, d100, mwf(15), mru).
+offering(comp4522, winter, d100, mwf(0), mru).
+offering(comp4630, winter, d100, mwf(11), mru).
 offering(comp4635, winter, d100, eve(tue), mru).
 offering(comp5690, winter, d100, eve(any), mru).
 offering(comp5690, fall, d100, eve(any), mru).
-offering(comp3612, fall, d100, mwf(3013), mru).
-
-
-offering(comp2521, fall, d100, tr2(110), mru).
-offering(comp2521, fall, d100, t2r(214), mru).
-offering(comp2521, winter, d100, tr2(315), mru).
-offering(comp3533, fall, d100, mwf(410), mru).
-offering(comp3533, winter, d100, mwf(510), mru).
-offering(comp3625, winter, d100, t2r(620), mru).
-offering(comp3505, fall, d100, mwf(713), mru).
-offering(comp4555, winter, d100, mwf(810), mru).
-offering(comp4513, fall, d100, mwf(915), mru).
-offering(comp4522, fall, d100, mwf(1110), mru).
-offering(comp4630, fall, d100, mwf(2011), mru).
-offering(comp4635, fall, d100, eve(tue), mru).
-offering(comp5690, fall, d100, eve(any), mru).
-offering(comp5690, winter, d100, eve(any), mru).
-offering(comp3612, winter, d100, mwf(3013), mru).
-
-required(comp1631).
-required(math1200).
-required(math1203).
-required(phil1179).
-required(comp3309).
-required(comp1633).
-required(math1271).
-required(math2234).
-required(comp2655).
-required(comp2631).
-required(comp2633).
-required(comp2613).
-required(comp2659).
-required(comp3614).
-required(comp3649).
-required(comp3659).
 
 prerequisites(comp1631, []).
 prerequisites(math1200, []).
@@ -113,48 +77,11 @@ prerequisites(comp4630, [comp3625]). %ml mwf(11)
 prerequisites(comp4635, [comp3533]). %dist eve(tue)
 prerequisites(comp5690, []). %sen eve(tue)
 
-
-
-has_offering([]).
-has_offering([X|XS]):- 
-    % write(X),nl,
-    offering(X, _, _, _, _),
-    has_offering(XS).
-
 jun_ops(Ops):- 
     Ops = [comp2521,comp3612,comp3533,comp3625,comp3505].
 
 sen_ops(Ops):-
     Ops = [comp4555,comp4513,comp4522,comp4630,comp4635,comp5690].
-
-has_three(SelectedOps):-
-    length(SelectedOps,L),
-    L >= 3.
-
-check_jun_ops(SelectedJOps):- 
-    jun_ops(Ops),
-    subset(SelectedJOps,Ops),
-    has_three(SelectedJOps).
-
-check_sen_ops(SelectedSOps,Required):- 
-    sen_ops(Ops),
-    subset(SelectedSOps,Ops),
-    has_three(SelectedSOps),
-    prerequisites_satisfied(SelectedSOps,Required).
-
-% TESTS
-% PASSING 
-% required_courses(D),append_options([comp2521,comp3612,comp3625],[comp4522,comp4513,comp4630],D,L).
-% FAILING
-% required_courses(D),append_options([comp2521],[comp4522],D,L).
-% required_courses(D),append_options([comp2521,comp3612],[comp4522,comp3612],D,L).
-append_options([],[],_,_):-false.
-append_options(Jun,Sen,Required,NewRequired):- 
-    check_jun_ops(Jun),
-    append(Jun,Required,RequiredNow),
-    check_sen_ops(Sen,RequiredNow),
-    append(Sen,RequiredNow,NewRequired).
-
 
 required_courses(C):-
     C=[
@@ -238,7 +165,7 @@ filterTakeable([C|Cs],Taken,CanTake):-
     prerequisites(C, Prereqlist),
     subset(Prereqlist, Taken),
     filterTakeable(Cs,Taken,CanTakeRest),
-    append([C],CanTakeRest,CanTake),!;
+    append([C],CanTakeRest,CanTake);
     filterTakeable(Cs,Taken,CanTake).
 
 choose(1, [H|_], [H]).
@@ -249,20 +176,6 @@ count(P,Count) :-
     findall(1,P,L),
     length(L,Count).
 
-% calcWeight(Pred,PW):-
-%     count(prerequisites(_, [Pred|_]),AsFirst),
-%     count(prerequisites(_, [_|[Pred]]),AsSecond),
-%     count(prerequisites(_, [_|[_|Pred]]),AsThird),
-%     L is AsFirst + AsSecond + AsThird,
-%     % count(prerequisites(_, [Pred|_]),L),
-%     (
-%         required(Pred),
-%         NewCount is L + 10,
-%         PW = Pred/NewCount
-%         ;
-%         PW = Pred/L
-%     ),!.
-    
 calcWeight(Pred,PW):-
     count(prerequisites(_, [Pred|_]),L),
     PW = Pred/L.
@@ -291,13 +204,11 @@ pick(Num,From,Too):-
     (
         choose(Num,From,TS);
         length(From,L),choose(L,From,TS)
-    ),!,
+    ),
     length(TS,Len),
     Len =< Num,
     Too = TS,!.
 
-prereq_depth().
-% optimal_pick(Num,From,Too):-
 
 feasible_semester_plan(_, [], _).
 feasible_semester_plan(Sem, Crses, Taken) :-
@@ -305,38 +216,30 @@ feasible_semester_plan(Sem, Crses, Taken) :-
     is_set(Crses),
     prerequisites_satisfied(Crses, Taken).
 
+
+
 semester_schedule(_, [], [],[],[]).
 semester_schedule(Sem, [Course|More], Schedule,Able,CourseSchedule) :-
   semester_schedule(Sem, More, Subschedule,SubAble,SubCourseSchedule),
-%   write(Course),nl,
   offering(Course, Sem, _, SchedPattern, Campus),
-%   write(Course),nl,
   schedule(SchedPattern, Campus, CourseSched),
-%   write(Course),nl,
   compatible_schedules(CourseSched, Subschedule),
-%   write(CourseSched),nl,
   append(CourseSched, Subschedule, Schedule),
-%   write(Schedule),nl,
 %   %write(idk),nl,
   append([Course], SubAble, Able),
-%   write(Able),nl,
-  append([course_instance(Course,CourseSched)],SubCourseSchedule,CourseSchedule)
-    % write(CourseSchedule),nl
-  ;
+  append([course_instance(Course,CourseSched)],SubCourseSchedule,CourseSchedule);
 %   %write("Cant"),nl,
   semester_schedule(Sem, More, Schedule,Able,CourseSchedule).
 
 prob_semester(_,0,_,_,[],[]).
 prob_semester(Sem,Num,Required,Taken,Able,CourseSchedule):-
-    subtract(Required,Taken,Crses),
+    (
+        subtract(Required,Taken,Crses)
+    ),
     filterTakeable(Crses,Taken,Takeable),
-    % write(Takeable),nl,
-    % sort_on_prereqs(Takeable,Sorted),!,
-    % pick(Num,Sorted,G),
-    % write(G),nl,
-    semester_schedule(Sem,Takeable,_,Able,CourseSchedule),
-    length(Able,L),
-    L =< Num.
+    sort_on_prereqs(Takeable,Sorted),!,
+    pick(Num,Sorted,G),
+    semester_schedule(Sem,G,_,Able,CourseSchedule).
 
 even(X) :- 0 is mod(X, 2).
 sem_type_select(Num,Sem):-
@@ -344,25 +247,7 @@ sem_type_select(Num,Sem):-
     Sem = winter,!;
     Sem = fall,!.
 
-% generate_plan(T1,T2,0,_,_):-T1\=T2,false.%write("FAILING"),nl,false.
-% generate_plan(T,T,0,_,[]).
-% generate_plan(T,T,_,_,[]).
-% generate_plan(Taken,Required,Sems,CrsPer,Semesters):-
-%     sem_type_select(Sems,Sem),
-%     prob_semester(Sem,CrsPer,Required,Taken,Able,CourseSchedule),
-%     append(Able,Taken,NewTaken),
-%     A = semester_plan(Sem,CourseSchedule),
-%     NextSems is Sems - 1,
-%     NextSems >= 0, 
-%     sort(NewTaken,  TakenSorted),
-%     sort(Required,  RequiredSorted),
-%     % write("HERE"),nl,
-%     generate_plan(TakenSorted,RequiredSorted,NextSems,CrsPer,SubSemesters),
-%     append([A],SubSemesters,Semesters).
-
-
-
-generate_plan(T1,T2,0,_,_):-T1\=T2,false.%write("FAILING"),nl,false.
+generate_plan(T1,T2,0,_,_):-T1\=T2,false.
 generate_plan(T,T,0,_,[]).
 generate_plan(T,T,_,_,[]).
 generate_plan(Taken,Required,Sems,CrsPer,Semesters):-
@@ -370,110 +255,16 @@ generate_plan(Taken,Required,Sems,CrsPer,Semesters):-
     prob_semester(Sem,CrsPer,Required,Taken,Able,CourseSchedule),
     append(Able,Taken,NewTaken),
     A = semester_plan(Sem,CourseSchedule),
-    NextSems is Sems - 1,
-    NextSems >= 0, 
     sort(NewTaken,  TakenSorted),
     sort(Required,  RequiredSorted),
-    % write("HERE"),nl,
+    NextSems is Sems - 1,
+    NextSems >= 0, 
     generate_plan(TakenSorted,RequiredSorted,NextSems,CrsPer,SubSemesters),
     append([A],SubSemesters,Semesters).
-% TEST CODE
-% PASSING
-% required_courses(D),test_student_choices(student(1,1,D)).
-% required_courses(D),test_student_choices(student(0,0,D)).
-% equired_courses(D),[_|XS]=D,test_student_choices(student(1,1,XS)).
-% FAILS
-% required_courses(D),[_|XS]=D,test_student_choices(student(0,1,XS)).
-% required_courses(D),[_|XS]=D,test_student_choices(student(0,0,XS)).
-% test_student_choices(student(1,1,[])).
-test_student_choices(Student):-
-    required_courses(Req),
-    test_student_courses_per(Student),
-    test_student_semesters(Student),
-    check_student_taken_is_sub(Student,Req),
-    check_for_impossible_times(Student,Req).
-% TEST CODE
-% PASSING
-% test_student_courses_per(student(_,0,_)).
-% test_student_courses_per(student(_,10,_)).
-% test_student_courses_per(student(_,-0,_)).
-% FAILS
-% test_student_courses_per(student(_,-10,_)).
-% test_student_courses_per(student(_,-1,_)).
-test_student_courses_per(student(_,CrsPer,_)):-
-    (
-        CrsPer >= 0,!
-        ;
-        !,write("Error: The Provided Maximum Courses Taken in a Semester is incorrrect"),nl,
-        tab(4),format("Message: Course Per Semester can not be negative, IE not (~w)",[CrsPer]),nl,
-        fail
-    ).
 
-% TEST CODE
-% PASSING
-% test_student_semesters(student(-0,_,_)).
-% test_student_semesters(student(10000000,_,_)).
-% test_student_semesters(student(1,_,_)).
-% FAILS
-% test_student_semesters(student(-1,_,_)).
-% test_student_semesters(student(-10,_,_)).
-% test_student_semesters(student(-10000000,_,_)).
-test_student_semesters(student(Sems,_,_)):-
-    (
-        Sems >= 0,!
-        ;
-        !,write("Error: The Provided Maximum Semesters is incorrect"),nl,
-        tab(4),format("Message: Maximum Semester Count can not be negative, IE not (~w)",[Sems]),nl,
-        fail
-    ).
-
-% TEST CODE
-% PASSING
-% required_courses(D),check_student_taken_is_sub(student(0,0,[comp1631]),D).
-% FAILS
-% required_courses(D),check_student_taken_is_sub(student(5,5,[comp10000]),D).
-check_student_taken_is_sub(student(_,_,Taken),Req):-
-    (
-        is_set(Taken),
-        subset(Taken,Req),!
-        ;
-        !,write("Error: The Provided Taken Courses are incorrect"),nl,
-        tab(4),write("Message: Taken classes are not a subset of the core required courses"),nl,
-        fail
-    ).
-% TEST CODE 
-% PASSING
-% required_courses(D),!,check_for_impossible_times(student(2,5,[comp1631,comp1633,comp2659,comp2655,math1200,math1203]),D).
-% FAILS
-% required_courses(D),!,check_for_impossible_times(student(2,5,[comp1631,comp1633,comp2659,comp2655]),D).
-% required_courses(D),!,check_for_impossible_times(student(2,5,[comp1631,comp1633]),D).
-% required_courses(D),!,check_for_impossible_times(student(2,5,[]),D).
-% required_courses(D),!,check_for_impossible_times(student(1,5,[]),D).
-% required_courses(D),!,check_for_impossible_times(student(0,5,[]),D).
-check_for_impossible_times(student(Sems,CrsPer,Taken),Req):-
-    TotalTakeableInTime is Sems * CrsPer,
-    subtract(Req,Taken,Needs),
-    length(Needs,NeedsLen),!,
-    (
-        TotalTakeableInTime >= NeedsLen,!
-        ;
-        !,write("Error: Not Enough Time To Complete Required Courses"),nl,
-        tab(4),format("Message: The maximum semesters provided (~w) and maximum courses per semester (~w) multiplied equals: ~w",[Sems,CrsPer,TotalTakeableInTime]),
-        tab(6),format("Which is larger then the number of still needed courses (~w): (~w >= ~w)",[NeedsLen,NeedsLen,TotalTakeableInTime]),
-        fail
-    ).
-
-
-    % graduation_plan_ops(student(20,10,[],[comp2521,comp3612,comp3625],[comp4522,comp4513,comp4630]),L).
-    % graduation_plan_ops(student(10,5,[comp1631,comp1633,comp2521,comp2613,comp2631,comp2633,comp2655,comp2659,comp3309,comp3614,comp3625,comp3649,comp4522,comp4630,math1200,math1203,math1271,math2234,phil1179],[comp2521,comp3612,comp3625],[comp4522,comp4513,comp4630]),L).
-graduation_plan_ops(student(Sems,CrsPer,Taken,Jun,Sen),Semesters):-
-    required_courses(Req),
-    append_options(Jun,Sen,Req,NewReq),
-    generate_plan(Taken,NewReq,Sems,CrsPer,Semesters).    
 
 graduation_plan(student(Sems,CrsPer,Taken),Semesters):-
     required_courses(Req),
-    % append_options(Jun,Sen,Req,NewReq),
     generate_plan(Taken,Req,Sems,CrsPer,Semesters).
 
 graduation_plan(Taken,Sems,CrsPer,Semesters):-
@@ -482,7 +273,7 @@ graduation_plan(Taken,Sems,CrsPer,Semesters):-
 
 write_lines(_,[]).
 write_lines(Out,[L|Ls]):-
-    tab(2),write(L),
+    write(L),
     write(Out,L),
     write_lines(Out,Ls).
 
@@ -555,13 +346,11 @@ test_plan(L):-
 
 main:-
     main("default.json").
-
 main(FileNameAndPath):-
     (
         exists_file(FileNameAndPath),
         get_student_dict(FileNameAndPath,StudentDict),
         get_student(StudentDict,Student)
-        
     ;
         tab(2),write("Error: Failed to Load Provided Json File."),nl,
         tab(4),write("- Ensure it is in the same directory or that the path is correct"),nl,
@@ -575,12 +364,11 @@ main(FileNameAndPath):-
         fail
     ),!,
     (
-        test_student_choices(Student),
         graduation_plan(Student,L),
         print_graduation_plan(0,L,Lines),
         nl,write("Generated a Plan"),nl,
         tab(2),write("Found plans are automatically appened to output.csv"),nl,nl,
-        write_plan(Lines),nl,
+        write_plan(Lines),
         write("Press (;) to continue"),nl,
         write("Press (.) to stop"),nl,nl
         ;
